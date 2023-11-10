9a8919619908e1ca99459e52dd1d89dd4c4b3b9f
Jan-Kaczmarek <jankaczmarek@opoczta.pl>
2023-11-10 09:46:31 +0100

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Menadżer_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void boxUser_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void boxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnCreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 objwindow1 = new Window1();
+            this.Visibility = Visibility.Hidden;
+            objwindow1.Show();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+    }
+
+}
