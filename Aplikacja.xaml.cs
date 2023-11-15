d4e676626b6f581a2c5fbc113a1ffb0b27b31388
Jan-Kaczmarek <jankaczmarek@opoczta.pl>
2023-11-15 14:07:37 +0100

    add datagrid

--- Aplikacja.xaml.cs
+++ Aplikacja.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.IO;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,8 +26,39 @@ namespace Menadżer_3
             InitializeComponent();
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
+            string filePath = "C:\\Users\\jan.kaczmarek\\source\\repos\\Menadżer 3\\Dane\\DaneLogowania.txt";
+            DataTable dt = new DataTable();
+
+            string[] lines = File.ReadAllLines(filePath);
+            if (lines.Length > 0)
+            {
+                // Pierwsza linia to nagłówki
+                string firstLine = lines[0];
+                string[] headerLabels = firstLine.Split('\t');
+
+                foreach (string header in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(header));
+                }
+
+                // Pozostałe linie to dane
+                for (int r = 1; r < lines.Length; r++)
+                {
+                    string[] items = lines[r].Split('\t');
+                    dt.Rows.Add(items);
+                }
+            }
+
+            // Przypisz DataTable do DataGrid
+            dataGrid.ItemsSource = dt.DefaultView;
+        }
+
+        private void Nteczka_Click(object sender, RoutedEventArgs e)
+        {
+            Nowateczka Nowateczka = new Nowateczka();
+            Nowateczka.Show();
         }
     }
 }
---
+++ Nowateczka.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Menadżer_3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Nowateczka.xaml
+    /// </summary>
+    public partial class Nowateczka : Window
+    {
+        public Nowateczka()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = "C:\\Users\\jan.kaczmarek\\source\\repos\\Menadżer 3\\Dane\\DaneLogowania.txt";
+            string webname = WebsiteName.Text;
+            string email = Email.Text;
+            string username = UserName.Text;
+            string password = Password.Text;
+
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine("Dane Logowania");
+                writer.WriteLine("Nazwa Strony: " + webname);
+                writer.WriteLine("email: " + email);
+                writer.WriteLine("Nazwa Użytkownika: " + username);
+                writer.WriteLine("Hasło: " + password);
+            }
+            this.Close();
+        }
+    }
+}
